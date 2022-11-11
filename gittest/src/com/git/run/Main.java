22619cf2732b44bdc2f8762243f23e7521bdf61f
GDJ56 <GDJ56@DESKTOP-HE2L8DR>
2022-11-11 16:43:59 +0900

    04_bs main 더하기 연산, 설정파일 추가

---
+++ gittest/src/com/git/run/Main.java
@@ -0,0 +1,14 @@
+package com.git.run;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello git");
+		
+		int a = 10;
+		int b = 20;
+		System.out.println(a+b);
+	}
+
+}
