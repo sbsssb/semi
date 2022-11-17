67c8c452f6399341f9ad3c85291fd522ef3ebdd3
GDJ56 <GDJ56@DESKTOP-HE2L8DR>
2022-11-17 09:44:58 +0900

    main 9,10라인 수정

--- gittest/src/com/git/run/Main.java
+++ gittest/src/com/git/run/Main.java
@@ -7,9 +7,8 @@ public class Main {
 		System.out.println("hello git");
 		
 		int a = 10;
-		int b = 20;
-		System.out.println(a + b);
-		
+		String test = "유병승";
+		System.out.println(a + test);
 		
 	
 	}
